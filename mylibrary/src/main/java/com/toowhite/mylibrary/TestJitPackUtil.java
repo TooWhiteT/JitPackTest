0b541c7a3ea82d0f1f49786c1b2365b16e95b398
TooWhiteHeroic <18627382757@163.com>
2020-10-24 00:05:15 +0800

    新增工具类一个

---
+++ mylibrary/src/main/java/com/toowhite/mylibrary/TestJitPackUtil.java
@@ -0,0 +1,18 @@
+package com.toowhite.mylibrary;
+
+/*
+ Created by tanzg on 2020/10/23.
+ Email: tzg1064690854@gmail.com
+  _______   ______   _____     _   _   ____      _ 
+ |__   __| |___  /  / ____|   | \ | | |  _ \    | |
+    | |       / /  | |  __    |  \| | | |_) |   | |
+    | |      / /   | | |_ |   | . ` | |  _ <    | |
+    | |     / /__  | |__| |   | |\  | | |_) |   |_|
+    |_|    /_____|  \_____|   |_| \_| |____/    (_)  
+*/
+public class TestJitPackUtil {
+    private final static String TAG = TestJitPackUtil.class.getSimpleName();
+    public void getVersion() {
+        System.out.println("TestJitPackUtil V1.0.1");
+    }
+}
